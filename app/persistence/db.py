4c237bcea435eabef4e05f162a116d8a6af88978
Nakul443 <nakul.kumar627@gmail.com>
2026-07-24 11:44:31 +0000

    initialized db connection

---
+++ app/persistence/db.py
@@ -0,0 +1,25 @@
+# database connection logic and session management
+
+import os
+from dotenv import load_dotenv
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from psycopg_pool import AsyncConnectionPool
+
+load_dotenv()
+
+
+DB_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+
+# asynchronous connection pool for PostgreSQL
+connection_pool = AsyncConnectionPool(
+    conninfo=DB_URI,
+    max_size=20,
+    open=False,
+)
+
+async def get_checkpointer() -> AsyncPostgresSaver:
+    """
+    Creates and returns an instance of AsyncPostgresSaver using the connection pool.
+    """
+    checkpointer = AsyncPostgresSaver(connection_pool)
+    return checkpointer
